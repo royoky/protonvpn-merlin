34477b819c6efba7e1e6491c404f96a9db88afff
Ronan Marzin <ronan.marzin@protonmail.com>
2022-04-14 21:07:13 +0200

    manage NetShield option add server label to username to connect to correct server prepare for wireguard management

--- select-server.js
+++ select-server.js
@@ -5,11 +5,11 @@ const readline = require("readline/promises").createInterface({
   output: process.stdout,
 });
 
-const api = "https://api.protonmail.ch/vpn/logicals";
+const api = "https://api.protonvpn.ch/vpn/logicals";
 
 let serverList;
 
-const getServers = () => {
+function getServers() {
   return fetch(api)
     .then((response) => {
       return response.json();
@@ -19,9 +19,9 @@ const getServers = () => {
       return serverList;
     })
     .catch((err) => console.log(err));
-};
+}
 
-const getCountryServers = (country = "FR") => {
+function getCountryServers(country = "FR") {
   return getServers().then((servers) => {
     return (countryServers = servers.filter(
       (server) =>
@@ -32,11 +32,11 @@ const getCountryServers = (country = "FR") => {
         server.Features === 8
     ));
   });
-};
+}
 
-const sortServers = (servers) => {
-  return servers.sort((a, b) => a.Load - b.Load);
-};
+function sortServers(servers) {
+  return servers.sort((a, b) => a.Score - b.Score);
+}
 
 let countryChosen;
 let listCountryCodes = [
@@ -102,7 +102,7 @@ let listCountryCodes = [
   "ZA",
 ];
 
-const getCountryList = () => {
+function getCountryList() {
   return getServers().then((servers) => {
     return servers.reduce((acc, curr, idx) => {
       if (idx === 0) {
@@ -116,18 +116,45 @@ const getCountryList = () => {
       return acc;
     }, []);
   });
-};
+}
 
-const changeServer = async () => {
+async function changeServer() {
   // Ask for country
   let country = await readline.question("Country to connect to? ");
   if (!country) {
     country = "FR";
   }
+  country = country.toUpperCase();
   if (!listCountryCodes.includes(country)) {
     throw new Error("Country not available");
   }
   console.log(`You chose to connect to ${country}`);
+
+  let protocol = await readline.question(
+    "Which protocol: OpenVpn (1) or Wireguard (2) (default (1)) ? :: "
+  );
+  if (!protocol) {
+    protocol = "1";
+  }
+  if (!["1", "2"].includes(protocol)) {
+    throw new Error("Enter 1 or 2");
+  }
+  console.log(
+    `Using ${protocol === "1" ? "OpenVpn" : "Wireguard"} protocol ...`
+  );
+
+  let netShield = "0";
+  if (protocol === "1") {
+    netShield = await readline.question(
+      "Do you want to enable ProtonVpn ad blocker (NetShield) (default No) ? \nNo (0)\nAnti-malware filtering (1)\nAd-blocking filtering (2)\n:: "
+    );
+    if (!netShield) {
+      netShield = "0";
+    }
+    if (!["0", "1", "2"].includes(netShield)) {
+      throw new Error("Enter 0, 1 or 2");
+    }
+  }
 
   const countryServers = await getCountryServers(country);
   const sortedServers = sortServers(countryServers);
@@ -136,41 +163,66 @@ const changeServer = async () => {
       acc.concat({
         name: server.Name,
         domain: server.Domain,
-        ip: server.Servers[0].EntryIP,
+        entryIp: server.Servers[0].EntryIP,
+        exitIp: server.Servers[0].ExitIP,
+        label: server.Servers[0].Label,
         load: server.Load,
+        key: server.Servers[0].X25519PublicKey,
+        score: server.Score,
       }),
     []
   );
 
-  // Display top best servers
-  console.log(`Here are the top10 best servers for country ${country} ::`);
-  for (let i = 0; i < 10; i++) {
+  // Display best servers
+  const noOfServers = 25;
+  console.log(
+    `Here are the top${noOfServers} servers for country ${country} ::`
+  );
+  for (let i = 0; i < noOfServers; i++) {
     console.log(
-      `Server ${i + 1} :: ${reduceServers[i].name} (${reduceServers[i].ip})`
+      `Server ${i + 1} :: ${reduceServers[i].name} | Entry IP: (${
+        reduceServers[i].entryIp
+      }) | Exit IP: (${reduceServers[i].exitIp})| Public Key: ${
+        reduceServers[i].key
+      } | ${reduceServers[i].load}% loaded | label: ${reduceServers[i].label}`
     );
   }
 
   const serverNumber = await readline.question(
-    "Which server do you want to connect to? (type a number between 1 and 10 :: "
+    `Which server do you want to connect to? (type a number between 1 and ${noOfServers} :: `
   );
   console.log(
     `VPN will connect to server ${reduceServers[serverNumber - 1].name} (ip: ${
-      reduceServers[serverNumber - 1].ip
+      reduceServers[serverNumber - 1].entryIp
     })`
   );
   readline.close();
 
-  exec(
-    `./create-ssh-script.sh ${reduceServers[serverNumber - 1].ip} ${reduceServers[serverNumber - 1].name.replace("#", "")}`,
-    (error, stdout, stderr) => {
-      console.log(stdout);
-      console.log(stderr);
-      if (error !== null) {
-        console.log(`exec error: ${error}`);
-      }
+  let command = "";
+  if (protocol === "1") {
+    command = `./create-ssh-script-ovpn.sh ${
+      reduceServers[serverNumber - 1].entryIp
+    } ${reduceServers[serverNumber - 1].name.replace("#", "")} ${netShield} ${
+      reduceServers[serverNumber - 1].label
+    }`;
+  } else {
+    if (reduceServers[serverNumber - 1].key.includes("/")) {
+      reduceServers[serverNumber - 1].key.replace("/", "/");
     }
-  );
-};
+    command = `./create-ssh-script-wg.sh ${reduceServers[
+      serverNumber - 1
+    ].name.replace("#", "")} ${reduceServers[serverNumber - 1].entryIp} ${
+      reduceServers[serverNumber - 1].key
+    }`;
+  }
+  exec(command, (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
+}
 /* return getCountryList()
   .then(list => {
     list.sort();
